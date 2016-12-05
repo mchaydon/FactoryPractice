d7f34b3e0e61f8c74d115d8d7faef4489ca0ce90
Mike <Mike@10.30.20.149>
2016-12-04 22:18:38 -0600

    Completed Lab

---
+++ src/factorypractice/labtwo/ServiceQuality.java
@@ -0,0 +1,9 @@
+package factorypractice.labtwo;
+
+/**
+ *
+ * @author jlombardo
+ */
+public enum ServiceQuality {
+    GOOD, FAIR, POOR
+}
---
+++ src/factorypractice/labtwo/TipCalculator.java
@@ -0,0 +1,19 @@
+package factorypractice.labtwo;
+
+/**
+ *
+ * @author jlombardo
+ */
+public interface TipCalculator {
+
+    public abstract double getTip();
+
+    public abstract Object getInputData();
+    
+    public abstract void setInputData(Object inputData);
+    
+    public abstract void setServiceQuality(ServiceQuality q);
+    
+    public abstract ServiceQuality getServiceQuality();
+    
+}
